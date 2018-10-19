a5497c315a454db140ccb677df1fe5bdbb63f95d
ownagebanana <adammills67@hotmail.com>
2018-10-19 15:12:04 -0300

    Initial work on Project initialization. Added profiling an db tools Inital data models

---
+++ HalifaxDine/HalifaxDine/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HalifaxDine.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int Emp_id { get; set; }
+        public int Privilege_id { get; set; }
+        public int Branch_id { get; set; }
+    }
+}
