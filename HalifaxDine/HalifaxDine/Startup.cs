e94fd0a6e48987993c56ac2a5ee21fc8ea9aefc2
ownagebanana <adammills67@hotmail.com>
2018-10-19 12:22:16 -0300

    Initial commit

---
+++ HalifaxDine/HalifaxDine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HalifaxDine.Startup))]
+namespace HalifaxDine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
