ab3602a774c65dd2f67d51e7258e9290bd34d365
ownagebanana <adammills67@hotmail.com>
2018-11-30 12:47:16 -0400

    BI work part two: electric boogaloo. Overall branch popularity

--- HalifaxDine/HalifaxDine/Controllers/BIController.cs
+++ HalifaxDine/HalifaxDine/Controllers/BIController.cs
@@ -24,26 +24,56 @@ namespace HalifaxDine.Controllers
             return View();
         }
 
-        public ActionResult PopularityData()
+        public ActionResult MonthlyPopularityData()
         {
 
             IEnumerable<PopularityModel> model = dao.GetBranchMonthlyPopularity();
-            var monthDict = model.GroupBy(x => x.Month, x => x, (key, g) => new { Month = key, vals = g.ToList() }).OrderBy(x=>x.Month);
+
+            //this makes a dictionary where month int is the key and the val is the list of objects which share that month val.
+            var monthDict = model.GroupBy(x => x.Month, x => x, (key, g) => new { Month = key, vals = g.ToList() }).OrderBy(x => x.Month);
 
             List<PopularityViewModel> result = new List<PopularityViewModel>();
+            //converting the month data into the viewmodel for the chart
             foreach (var group in monthDict)
             {
                 PopularityViewModel tmp = new PopularityViewModel();
-                tmp.name = months[group.Month-1];
-                tmp.x = group.vals.Select(x=>x.Branch_Province).ToArray();
-                tmp.y = group.vals.Select(x=>x.Avg_Rating).ToArray();
+                tmp.name = months[group.Month - 1];
+                tmp.x = group.vals.Select(x => x.Branch_Province).ToArray();
+                tmp.y = group.vals.Select(x => x.Avg_Rating).ToArray();
 
                 result.Add(tmp);
             }
-            //model.Add("Overall", dao.GetBranchPopularity().ToList());
-            //model.Add("ItemPopularity", dao.GetBranchItemPopularity().ToList());
 
             return Json(result.ToArray<PopularityViewModel>(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult OverallPopularityData()
+        {
+
+            IEnumerable<PopularityModel> model = dao.GetBranchPopularity().OrderBy(x => x.Branch_Province);
+
+            PopularityViewModel result = new PopularityViewModel();
+
+            result.x = model.Select(x => x.Branch_Province).ToArray<string>();
+            result.y = model.Select(x => x.Avg_Rating).ToArray<float>();
+
+            //result needs to be in an array for the JS library
+            return Json(new PopularityViewModel[] { result }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult ItemPopularity()
+        {
+
+            IEnumerable<PopularityModel> model = dao.GetBranchItemPopularity();
+
+            PopularityViewModel result = new PopularityViewModel();
+
+            result.x = model.Select(x => x.Branch_Province).ToArray<string>();
+            result.y = model.Select(x => x.Avg_Rating).ToArray<float>();
+
+            //result needs to be in an array for the JS library
+            return Json(new PopularityViewModel[] { result }, JsonRequestBehavior.AllowGet);
         }
     }
 }
